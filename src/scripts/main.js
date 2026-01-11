f9655db9c303605a053de1cded756a66753aaa4d
Yar14k <tabachuk1406@gmail.com>
2026-01-11 15:51:05 +0100

    Add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,20 +3,21 @@
 const field = document.querySelector('.wall');
 const spider = document.querySelector('.spider');
 
-  document.addEventListener('click', (e) => {
+field.addEventListener('click', (e) => {
+  const fieldMore = field.getBoundingClientRect();
+  const spiderMore = spider.getBoundingClientRect();
 
- const fieldMore = field.getBoundingClientRect();
- const spiderMore = spider.getBoundingClientRect();
+  const widthX = getComputedStyle(field).borderLeftWidth;
+  const borderX = parseInt(widthX);
+  const heightY = getComputedStyle(field).borderTopWidth;
+  const borderY = parseInt(heightY);
 
-let x = e.clientX - fieldMore.left - spiderMore.width / 2;
-let y = e.clientY - fieldMore.top - spiderMore.height / 2;
+  let x = e.clientX - fieldMore.left - spiderMore.width / 2 - borderX;
+  let y = e.clientY - fieldMore.top - spiderMore.height / 2 - borderY;
 
-x = Math.max(0, Math.min(x, fieldMore.width - spiderMore.width));
-y = Math.max(0, Math.min(y, fieldMore.height - spiderMore.height));
+  x = Math.max(0, Math.min(x, field.clientWidth - spiderMore.width));
+  y = Math.max(0, Math.min(y, field.clientHeight - spiderMore.height));
 
-
-spider.style.left = x + 'px';
-spider.style.top = y + 'px';
-
- });
-
+  spider.style.left = x + 'px';
+  spider.style.top = y + 'px';
+});
