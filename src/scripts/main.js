3675923cf08f02b8076350aeaec5b63c7810b80b
Yar14k <tabachuk1406@gmail.com>
2026-01-11 13:08:25 +0100

    --no-verify

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,22 @@
 'use strict';
 
-document.addEventListener('click', (e) => {
-  // write code here
-});
+const field = document.querySelector('.wall');
+const spider = document.querySelector('.spider');
+
+  document.addEventListener('click', (e) => {
+
+ const fieldMore = field.getBoundingClientRect();
+ const spiderMore = spider.getBoundingClientRect();
+
+let x = e.clientX - fieldMore.left - spiderMore.width / 2;
+let y = e.clientY - fieldMore.top - spiderMore.height / 2;
+
+x = Math.max(0, Math.min(x, fieldMore.width - spiderMore.width));
+y = Math.max(0, Math.min(y, fieldMore.height - spiderMore.height));
+
+
+spider.style.left = x + 'px';
+spider.style.top = y + 'px';
+
+ });
+
